5ff82c85417aad458beaf7f418d0b5afb7bd7b68
Yashashwi Wahie <103446037+yashashwiwahie30@users.noreply.github.com>
2024-04-25 14:57:25 +0530

    Update Credentials.h

--- Speech_To_text/Credentials.h
+++ Speech_To_text/Credentials.h
@@ -8,11 +8,11 @@ const char *password = "Xsgd703*";// WiFi Password ( Keep it blank if your WiFi 
 
 
 // Parameters of Google Speech to Text API
-const String ApiKey = "AIzaSyD1uRXzlv-_6k7OuJ5QWSjC8DSKOKtPa5g";// Google Speech to Text API key
+const String ApiKey = "******************************";// Google Speech to Text API key
 String SpeechtoText_Language = "en-IN"; // Language 
 
 // Parameters of OpenAI API 
-const char* chatgpt_token = "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"; // OpenAI API Key
+const char* chatgpt_token = "******************************************"; // OpenAI API Key
 String OpenAI_Model = "gpt-3.5-turbo-instruct"; // Model
 String OpenAI_Temperature = "0.20"; // temperature
 String OpenAI_Max_Tokens = "40"; //Max Tokens 
